1dfb26c1510eab07e829d6c715ae0f57efd4a6e6
Jorgen Eriksen <jorgen.eriksen93@gmail.com>
2021-04-28 18:06:02 +0200

    setup discord bot

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module projectGroup23
 go 1.16
 
 require (
+	github.com/bwmarrin/discordgo v0.23.2 // indirect
 	github.com/kr/pretty v0.2.1
 	googlemaps.github.io/maps v1.3.2 // indirect
 )
--- main.go
+++ main.go
@@ -2,43 +2,58 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 )
 
-func simpleHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintf(w, "Hello This is a simple thing")
+func main() {
+	var err error
+
+	token := "ODM2OTgzNjUyMjUxMzM2Nzc1.YIl7xQ.cuxQXG5lW9Sqmylm6rx4INNiLpc"
+
+	var s, _ = discordgo.New("Bot " + token)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
+
+	s.AddHandler(messageCreate)
+	s.Identify.Intents = discordgo.IntentsGuildMessages
+
+	if err = s.Open(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit...")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	s.Close()
+
 }
 
-func main() {
-	/* 	c, err := maps.NewClient(maps.WithAPIKey("AIzaSyDyKyvfKQuu90-cieJZ2vPiK9u6n7DLWFE"))
-	   	if err != nil {
-	   		log.Fatalf("fatal error: %s", err)
-	   	}
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
-	   	r := &maps.DirectionsRequest{
-	   		Origin:      "Oslo",
-	   		Destination: "Trondheim",
-	   	}
-
-	   	route, _, err := c.Directions(context.Background(), r)
-	   	if err != nil {
-	   		log.Fatalf("fatal error: %s", err)
-	   	}
-
-	   	pretty.Println(route)
-	*/
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "drit!")
 	}
 
-	http.HandleFunc("/", simpleHandler)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 }
