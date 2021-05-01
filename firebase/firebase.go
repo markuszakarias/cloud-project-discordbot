45bb5c4b83bbd5d2ef36d340c8036fe5676f6108
Jorgen Eriksen <jorgen.eriksen93@gmail.com>
2021-05-01 19:11:48 +0200

    merging to master

---
+++ firebase/firebase.go
@@ -0,0 +1,61 @@
+package firebase
+
+import (
+	"context"
+	"log"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+)
+
+type Joke struct {
+	text string
+}
+
+var ctx context.Context
+var client *firestore.Client
+
+// initialize firebase/firestore
+func InitFirebase() {
+	ctx = context.Background()
+	opt := option.WithCredentialsFile("./firebase/firebasePrivateKey.json")
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err = app.Firestore(ctx)
+}
+
+func GetAllJokes() []string {
+	iter := client.Collection("jokes").Documents(ctx)
+	var allJokes []string
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return allJokes
+		}
+		//joke := doc.Data()["text"]
+		var test string = doc.Data()["text"].(string)
+		allJokes = append(allJokes, test)
+	}
+	return allJokes
+}
+
+func CreateJoke(userId string, jokeText string) error {
+	_, _, err := client.Collection("jokes").Add(ctx,
+		map[string]interface{}{
+			"createdBy": userId,
+			"text":      jokeText,
+		})
+	return err
+}
+
+func getAllJokesByUserId() {
+	//iter := client.Collection("jokes").Documents(ctx)
+}
--- main.go
+++ main.go
@@ -2,26 +2,99 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 	"os"
-	"projectGroup23/handlers"
+	"os/signal"
+	"projectGroup23/firebase"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
 
-	address := ":" + port
-	fmt.Print("Listening on ", address, "...\n")
+	/*
+		port := os.Getenv("PORT")
+		if port == "" {
+			port = "8080"
+		}
 
-	http.HandleFunc("/newsletter", handlers.GetDailyNewsLetter)
-	http.HandleFunc("/mealplan", handlers.GetDailyMealPlan)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+		address := ":" + port
+		fmt.Print("Listening on ", address, "...\n")
 
-	if err := http.ListenAndServe(address, nil); err != nil {
+		http.HandleFunc("/newsletter", handlers.GetDailyNewsLetter)
+		http.HandleFunc("/mealplan", handlers.GetDailyMealPlan)
+		log.Fatal(http.ListenAndServe(":"+port, nil))
+
+		if err := http.ListenAndServe(address, nil); err != nil {
+			panic(err)
+		}
+	*/
+
+	firebase.InitFirebase()
+
+	token := "ODM2OTgzNjUyMjUxMzM2Nzc1.YIl7xQ.cuxQXG5lW9Sqmylm6rx4INNiLpc"
+
+	var s, err = discordgo.New("Bot " + token)
+
+	if err != nil {
 		panic(err)
 	}
+
+	s.AddHandler(messageCreate)
+	s.Identify.Intents = discordgo.IntentsGuildMessages
+
+	if err = s.Open(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit...")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	s.Close()
+
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "drit!")
+	}
+
+	if m.Content == "jokes" {
+		jokes := firebase.GetAllJokes()
+		for _, a := range jokes {
+			s.ChannelMessageSend(m.ChannelID, a)
+		}
+	}
+
+	if strings.HasPrefix(m.Content, "!createjoke ") {
+		joke := m.Content[12:]
+		err := firebase.CreateJoke(s.State.User.ID, joke)
+		if err != nil {
+			s.ChannelMessageSend(m.ChannelID, err.Error())
+		} else {
+			s.ChannelMessageSend(m.ChannelID, "Joke created")
+		}
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
+
+	if m.Content == "minbrukerid" {
+		fmt.Println("asd")
+		s.ChannelMessageSend(m.ChannelID, s.State.User.ID)
+	}
+
 }
