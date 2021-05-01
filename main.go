4a6b4cbddfd1db74077315465445b9b7c8452277
Jorgen Eriksen <jorgen.eriksen93@gmail.com>
2021-05-01 18:31:16 +0200

    creating and showing jokes

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"projectGroup23/firebase"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -11,6 +13,8 @@ import (
 
 func main() {
 	var err error
+
+	firebase.InitFirebase()
 
 	token := "ODM2OTgzNjUyMjUxMzM2Nzc1.YIl7xQ.cuxQXG5lW9Sqmylm6rx4INNiLpc"
 
@@ -48,12 +52,33 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		s.ChannelMessageSend(m.ChannelID, "drit!")
 	}
 
+	if m.Content == "jokes" {
+		jokes := firebase.GetAllJokes()
+		for _, a := range jokes {
+			s.ChannelMessageSend(m.ChannelID, a)
+		}
+	}
+
+	if strings.HasPrefix(m.Content, "!createjoke ") {
+		joke := m.Content[12:]
+		err := firebase.CreateJoke(s.State.User.ID, joke)
+		if err != nil {
+			s.ChannelMessageSend(m.ChannelID, err.Error())
+		} else {
+			s.ChannelMessageSend(m.ChannelID, "Joke created")
+		}
+	}
+
 	// If the message is "pong" reply with "Ping!"
 	if m.Content == "pong" {
 		s.ChannelMessageSend(m.ChannelID, "Ping!")
 	}
 
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	if m.Content == "minbrukerid" {
+		fmt.Println("asd")
+		s.ChannelMessageSend(m.ChannelID, s.State.User.ID)
 	}
+
+	fmt.Println(s)
+
 }
--- utilities/utilities.go
+++ utilities/utilities.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+
 
 func helloPrinter() {
 	fmt.Println("Hello")
