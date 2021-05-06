16e2a876650e193a86f9884f2ec8fd64a6821698
Salvador Bascunan <sobascun@stud.ntnu.no>
2021-05-06 17:32:17 +0200

    re-written todolist, works with Azure sql

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 
 	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/go-sql-driver/mysql"
 )
+
+type todo_struct struct {
+	id       int64
+	userid   string
+	title    string
+	category string
+	state    string
+}
 
 var server = "vmdata.database.windows.net"
 var port = 1433
@@ -34,23 +43,150 @@ func main() {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	fmt.Printf("Connected!\n")
 
-	// Read employees
-	count, err := getTodos()
+	// Use this stuct to create/display/delete/update one todo object
+	var todoObject todo_struct
+	todoObject.userid = "002"
+	todoObject.title = "Second Task"
+	todoObject.category = "Second Category"
+	todoObject.state = "active"
+
+	// Read all todo
+	err = getTodoAll()
 	if err != nil {
-		log.Fatal("Error reading from DB: ", err.Error())
+		log.Fatal("Error reading all todo objects: ", err.Error())
 	}
-	fmt.Printf("Read %d row(s) successfully.\n", count)
+
+	// Read one todo
+	/* err = getTodoObject("abcdefgh12345678")
+	if err != nil {
+		log.Fatal("Error reading todo object: ", err.Error())
+	} */
+
+	// Create todo object
+	/*err = createTodoObject(todoObject)
+	if err != nil {
+		log.Fatal("Error creating todo object: ", err.Error())
+	} */
+
+	// Delete todo object
+	/* 	err = deleteTodoObject("abcdefgh12345678")
+	   	if err != nil {
+	   		log.Fatal("Error reading all todo objects: ", err.Error())
+	   	} */
+
+	// Update todo object
+	/* 	err = updateTodoObject(todoObject)
+	   	if err != nil {
+	   		log.Fatal("Error updating todo object: ", err.Error())
+	   	} */
 }
 
-func getTodos() (int, error) {
+func updateTodoObject(todoObject todo_struct) error {
+	ctx := context.Background()
+
+	if db == nil {
+		err = errors.New("create todo object: db is null")
+	}
+
+	// Check db connection
+	err = db.PingContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	tsql := "UPDATE [dbo].[todo] SET Category = @Category WHERE Userid = @Userid;"
+
+	// Execute non-query with named parameters
+	res, err := db.ExecContext(ctx, tsql,
+		sql.Named("Userid", todoObject.userid),
+		sql.Named("Category", todoObject.category))
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(res)
+
+	return nil
+}
+
+func deleteTodoObject(todoObject todo_struct) error {
+	ctx := context.Background()
+
+	if db == nil {
+		err = errors.New("create todo object: db is null")
+	}
+
+	// Check db connection
+	err = db.PingContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	tsql := "DELETE FROM [dbo].[todo] WHERE Userid = @Userid;"
+
+	// Execute non-query with named parameters
+	res, err := db.ExecContext(ctx, tsql,
+		sql.Named("Userid", todoObject.userid))
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(res)
+
+	return nil
+}
+
+func createTodoObject(todoObject todo_struct) error {
+	ctx := context.Background()
+
+	if db == nil {
+		err = errors.New("create todo object: db is null")
+	}
+
+	// Check db connection
+	err = db.PingContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	tsql := `INSERT INTO [dbo].[todo] (Userid, Title, Category, State) VALUES (@Userid, @Title, @Category, @State);
+			select isNull(SCOPE_IDENTITY(), -1);`
+
+	stmt, err := db.Prepare(tsql)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	row := stmt.QueryRowContext(ctx,
+		sql.Named("Userid", todoObject.userid),
+		sql.Named("Title", todoObject.title),
+		sql.Named("Category", todoObject.category),
+		sql.Named("State", todoObject.state))
+
+	fmt.Println("Before scan")
+
+	err = row.Scan(&todoObject.id)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("New ID was created:", todoObject.id)
+
+	return nil
+}
+
+func getTodoAll() error {
 	ctx := context.Background()
 
 	// Check database connection
 	err := db.PingContext(ctx)
 	if err != nil {
-		return -1, err
+		return err
 	}
 
 	// The sql query to be executed
@@ -59,13 +195,13 @@ func getTodos() (int, error) {
 	// Execute query
 	rows, err := db.QueryContext(ctx, tsql)
 	if err != nil {
-		return -1, err
+		return err
 	}
 
 	// Wait until function ends before
 	defer rows.Close()
 
-	var count int
+	fmt.Println("Print all todo data:")
 
 	// Iterate through the result set.
 	for rows.Next() {
@@ -75,53 +211,55 @@ func getTodos() (int, error) {
 		// Get values from row.
 		err := rows.Scan(&id, &userid, &title, &category, &state)
 		if err != nil {
-			return -1, err
+			return err
 		}
 
 		fmt.Printf("Id:%d Userid:%s Title: %s, Category: %s, State: %s\n", id, userid, title, category, state)
-		count++
 	}
 
-	return count, nil
+	return nil
 }
 
-/* 	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
-   		server, user, password, port, database)
+func getTodoObject(todoObject todo_struct) error {
+	ctx := context.Background()
 
-   	var err error
+	// Check database connection
+	err := db.PingContext(ctx)
+	if err != nil {
+		return err
+	}
 
-   	// Opening a database connection with tcp
-   	db, err := sql.Open("sqlserver", connString)
-   	if err != nil {
-   		log.Fatal(err)
-   	}
+	// The sql query to be executed
+	tsql := "SELECT Id, Userid, Title, Category, State FROM [dbo].[todo] WHERE Userid = @Userid;"
 
-   	ctx := context.Background()
-   	err = db.PingContext(ctx)
-   	if err != nil {
-   		log.Fatal(err.Error())
-   	}
-   	fmt.Printf("Connected!\n")
+	// Execute query
+	rows, err := db.QueryContext(ctx, tsql, sql.Named("UserId", todoObject.userid))
+	if err != nil {
+		return err
+	}
 
-   	count, err := getTodos()
-   	if err != nil {
-   		log.Fatal("Error reading Employees: ", err.Error())
-   	}
-   	fmt.Printf("Read %d row(s) successfully.\n", count) */
+	// Wait until function ends before
+	defer rows.Close()
 
-// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
-// db.SetMaxIdleConns(100)
-// Wait until the end of the function to close the connection
-// defer db.Close()
+	fmt.Println("Print one todo object:")
 
-// Creating an instance of the struct
-//db_server := &utils.DB_server{DB: db}
-//appHandler := &utils.RegexHandler{}
+	// Iterate through the result set.
+	for rows.Next() {
 
-//fmt.Println(db_server)
-//fmt.Println(appHandler)
+		// Get values from row.
+		err := rows.Scan(&todoObject.id,
+			&todoObject.userid,
+			&todoObject.title,
+			&todoObject.category,
+			&todoObject.state)
 
-//fmt.Println("Listening on port 8080")
-//http.ListenAndServe(":8080", appHandler)
+		if err != nil {
+			return err
+		}
 
-// Opening a database connection with tcp
+		fmt.Printf("Id:%d Userid:%s Title: %s, Category: %s, State: %s\n",
+			todoObject.id, todoObject.userid, todoObject.title, todoObject.category, todoObject.state)
+	}
+
+	return nil
+}
